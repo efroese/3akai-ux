3cd4f5f1273ae53efd5a94c27edb636427299885
Erik Froese <erik.froese@gmail.com>
2012-04-05 13:54:40 -0400

    ACAD-832 Add config option to disable the email verify dialog.

--- dev/configuration/config_custom.js
+++ dev/configuration/config_custom.js
@@ -4,6 +4,8 @@ define(["config/config"], function(config) {
      * rSmart default customizations
      */
 
+    config.emailverify_disabled = false;
+
     // Add some custom URLs
     config.URL.SEARCH_USERS_GROUPS_KNOWN = "/var/search/usersgroups-known.json";
     config.URL.USER_EMAIL_EXISTENCE_SERVICE = "/system/userManager/user.emailexists.html?email=__EMAIL__";
